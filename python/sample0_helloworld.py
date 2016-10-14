e3e6499ae67547c7e10ae87576d571c93c909002
Clément Pellegrini <clement.pellegrini@qarnot-computing.com>
2016-10-14 11:11:45 +0200

    Use configuration file in sample0.

--- python/sample0_helloworld.py
+++ python/sample0_helloworld.py
@@ -1,5 +1,5 @@
 import qarnot
-conn = qarnot.connection.Connection({'client_auth': 'xxxx-mytoken-xxxx'})
+conn = qarnot.connection.Connection('samples.conf')
 task = conn.create_task('sample0_helloworld', 'docker-batch', 1)
 task.constants['DOCKER_CMD'] = 'echo hello world!'
 task.run()
