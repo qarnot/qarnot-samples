5776a365b0936889fcba41ee6d8a996bba08d7ee
Hugo Rybinski <hugo.rybinski@qarnot-computing.com>
2016-12-05 17:44:04 +0100

    use argparser for sample 4

--- python/sample4_ffmpeg.py
+++ python/sample4_ffmpeg.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
- 
+import argparse
 import sys
 import qarnot
 
 # Parse ffmpeg command line
 
-# Check if some arguments were provided
+# Create a argument parser to parse input files
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', action='append', required=True)
+
+# Get arguments
 args = sys.argv[1:]
-if len(args) == 0:
-    sys.stderr.write("usage: %s [ffmpeg arguments]\n" % sys.argv[0])
-    sys.exit(1)
+
+# Parse input files and store them as a list
+input_files = parser.parse_known_args(args)[0].i
 
 # Build the full command line
 ffmpeg_cmd = ' '.join(args)
 
-# Find the input file
-input_file = None
-for i in range(0, len(args)-1):
-  if args[i] == "-i": input_file = args[i+1]
-
 # Display that we parsed
 print("** FFMPEG command: %s" % ffmpeg_cmd)
-print("** Input file: %s" % input_file)
- 
+print("** Input files: %s" % ', '.join(input_files))
+
 # Edit 'samples.conf' to provide your own credentials
- 
+
 # Create a connection, from which all other objects will be derived
 conn = qarnot.Connection('samples.conf')
 
@@ -35,25 +34,26 @@ task = conn.create_task('sample4-ffmpeg', 'docker-batch', 1)
 
 # Store if an error happened during the process
 error_happened = False
+
 try:
     # Set the command to run when launching the container, by overriding a
     # constant.
     # Task constants are the main way of controlling a task's behaviour
     task.constants['DOCKER_REPO'] = 'jrottenberg/ffmpeg'
     task.constants['DOCKER_CMD'] = ffmpeg_cmd
-    #task.constants['DOCKER_CMD'] = "sleep 3600"
+    # task.constants['DOCKER_CMD'] = "sleep 3600"
 
-    if input_file != None:
+    input_disk = conn.create_disk('sample4-ffmpeg-input-resource')
+    for input_file in input_files:
         # Create a resource disk and add our input file.
-        input_disk = conn.create_disk('sample4-ffmpeg-input-resource')
         input_disk[input_file] = input_file
-    
-        # Attach the disk to the task
-        task.resources.append(input_disk)
+
+    # Attach the disk to the task
+    task.resources.append(input_disk)
 
     # Submit the task to the Api, that will launch it on the cluster
     task.submit()
- 
+
     # Wait for the task to be finished, and monitor the progress of its
     # deployment
     last_state = ''
@@ -62,22 +62,21 @@ try:
         if task.state != last_state:
             last_state = task.state
             print("** {}".format(last_state))
-  
+
         # Wait for the task to complete, with a timeout of 5 seconds.
         # This will return True as soon as the task is complete, or False
         # after the timeout.
         done = task.wait(5)
- 
+
         # Display fresh stdout / stderr
         sys.stdout.write(task.fresh_stdout())
         sys.stderr.write(task.fresh_stderr())
- 
+
     if task.state == 'Failure':
         # Display errors on failure
         print("** Errors: %s" % task.errors[0])
         error_happened = True
-
-    else: 
+    else:
         # Or download the results
         task.download_results('.')
 
@@ -86,5 +85,3 @@ finally:
     # Exit code in case of error
     if error_happened:
         sys.exit(1)
-    pass
-
