cf31d052ff98e693caf290d95fea751cad7c3744
Yoann Ricordel <yoann.ricordel@qarnot-computing.com>
2021-04-30 14:20:15 +0200

    Fix Archlinux image name
    And some trailing whitespaces to go with it.

--- python/sample3_dockerhub.py
+++ python/sample3_dockerhub.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
- 
+
 import sys
 import qarnot
 import os
 import operator
 
 # Edit 'samples.conf' to provide your own credentials
- 
+
 # Create a connection, from which all other objects will be derived
 conn = qarnot.Connection('samples.conf')
 
@@ -18,8 +18,8 @@ linux_versions = ["library/centos:5",      \
                   "library/ubuntu:16.04",  \
                   "library/debian:jessie", \
                   "opensuse/leap:42.3", \
-                  "archlinux/base:latest"]
- 
+                  "archlinux:latest"]
+
 # Create the tasks
 tasks = {i: conn.create_task('sample3-dockerhub-%s' % i, 'docker-batch', 1) for i in linux_versions}
 
@@ -35,9 +35,9 @@ try:
         task.constants['DOCKER_TAG'] = tag
         task.constants['DOCKER_CMD'] = 'sh -c "cat /etc/issue | head -n 1"'
         print("** Submitting %s..." % task.name)
- 
+
         # Submit the task to the Api, that will launch it on the cluster
-        task.submit()    
+        task.submit()
 
     # Wait for the task to be finished, and monitor the progress of its
     # deployment
@@ -48,16 +48,16 @@ try:
             if task.state != last_state[task.name]:
                 last_state[task.name] = task.state
                 print("** {} >>> {}".format(task.name, last_state[task.name]))
- 
+
         # Wait for the task to complete, with a timeout of 2 seconds.
         # This will return True as soon as the task is complete, or False
-        # after the timeout.        
+        # after the timeout.
         done = all([task.wait(2) for task in tasks.values()])
- 
+
         # Display fresh stdout / stderr
         map(sys.stdout.write, [task.fresh_stdout() for task in tasks.values()])
         map(sys.stderr.write, [task.fresh_stderr() for task in tasks.values()])
- 
+
     # Display errors on failure
     for task in tasks.values():
         if task.state == 'Failure':
